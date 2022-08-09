8ade092893d14e97d12af216e6eca2c49d3a3f8a
pingmado <75196272+ping2109@users.noreply.github.com>
2022-08-09 21:29:13 +0700

    a505f: Gapps build

--- spark_a505f.mk
+++ spark_a505f.mk
@@ -33,6 +33,9 @@ $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Gapps
+WITH_GAPPS := true
+
 ## Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := a505f
 PRODUCT_NAME := spark_a505f
