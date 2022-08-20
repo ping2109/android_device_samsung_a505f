6dd9634001a8e20cc3d9244fb078f29635e8fe0b
Anh Vinh <75196272+ping2109@users.noreply.github.com>
2022-08-20 09:53:23 +0700

    a505f: POSP bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a505f.mk
+    $(LOCAL_DIR)/potato_a505f.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a505f-eng \
-    lineage_a505f-user \
-    lineage_a505f-userdebug
+    potato_a505f-eng \
+    potato_a505f-user \
+    potato_a505f-userdebug
